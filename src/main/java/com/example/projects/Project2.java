91e65d35053ffa9c9763e838d0acbfce83d6491d
brian <brian@joinblink.com>
2016-09-10 13:13:57 +0100

    initial commit

---
+++ src/main/java/com/example/projects/Project2.java
@@ -0,0 +1,11 @@
+package com.example.projects;
+
+public class Project2 {
+    public static void main(String[] args) {
+        System.out.println("Hello from project2");
+    }
+
+    public static String printFromProject2(){
+        return "hi from project 2";
+    }
+}
